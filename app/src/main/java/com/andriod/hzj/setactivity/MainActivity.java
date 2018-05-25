384556ca627eadc0316d46aa23b87e24764c7e37
hzjkaka <38029651+hzjkaka@users.noreply.github.com>
2018-05-25 18:35:04 +0800

    Initial commit

---
+++ app/src/main/java/com/andriod/hzj/setactivity/MainActivity.java
@@ -0,0 +1,28 @@
+package com.andriod.hzj.setactivity;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.preference.PreferenceFragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FragmentManager mFragmentManager = getFragmentManager();
+        FragmentTransaction mFragmentTransaction = mFragmentManager
+                .beginTransaction();
+        PrefsFragment mPrefsFragment = new PrefsFragment();
+        mFragmentTransaction.replace(android.R.id.content, mPrefsFragment);
+        mFragmentTransaction.commit();
+    }
+
+    public static class PrefsFragment extends PreferenceFragment {
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+        }
+    }
+}
